2d527a679243e3f6f3fe9d0409929f710f12f629
João Pedro Truchinski Borba <joao.borba@asaas.com.br>
2026-03-03 13:17:34 -0300

    upd: salva o endpoint do RDS no Parameter Store para que o APP consuma de forma independente

--- infra/ssm_parameter.tf
+++ infra/ssm_parameter.tf
@@ -3,3 +3,9 @@ resource "aws_ssm_parameter" "db_endpoint" {
     type  = "String"
     value = aws_db_instance.postgres.endpoint
 }
+
+resource "aws_ssm_parameter" "db_secret_arn" {
+    name  = "/garage/prod/db/secret_arn"
+    type  = "String"
+    value = length(aws_db_instance.postgres.master_user_secret) > 0 ? aws_db_instance.postgres.master_user_secret[0].secret_arn : ""
+}
